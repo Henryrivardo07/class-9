dafdd84afbb802aa8d71c88d4f9a948cb770c8f2
henry rivardo <henryrivardo@outlook.com>
2025-03-12 19:54:52 +0700

    update 3

--- 4F.prototype-Constructor-Function/3.Membuat-Inheritance-dengan-Constructor-Function/index.js
+++ 4F.prototype-Constructor-Function/3.Membuat-Inheritance-dengan-Constructor-Function/index.js
@@ -1,15 +1,26 @@
+function Person(name) {
+  this.name = name;
+}
+
+Person.prototype.sayHello = function () {
+  console.log(`Hi, I'm ${this.name}`);
+};
+
 function Employee(name, job) {
-  Person.call(this, name);
+  Person.call(this, name); // ✅ Memanggil konstruktor Person
   this.job = job;
 }
 
+// Pewarisan dari Person
 Employee.prototype = Object.create(Person.prototype);
 Employee.prototype.constructor = Employee;
 
+// Method tambahan untuk Employee
 Employee.prototype.work = function () {
   console.log(`${this.name} is working as a ${this.job}`);
 };
 
+// Membuat instance Employee
 const emp = new Employee("John", "Developer");
-emp.sayHello(); // Output: Hi, I'm John
-emp.work(); // Output: John is working as a Developer
+emp.sayHello(); // ✅ Output: Hi, I'm John
+emp.work(); // ✅ Output: John is working as a Developer
