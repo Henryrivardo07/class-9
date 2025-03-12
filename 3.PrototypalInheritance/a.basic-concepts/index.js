4b61aca826cc07d078fd3c332b7f52217605e0ff
henry rivardo <henryrivardo@outlook.com>
2025-03-12 19:49:30 +0700

    update 2

--- 3.PrototypalInheritance/a.basic-concepts/index.js
+++ 3.PrototypalInheritance/a.basic-concepts/index.js
@@ -1,33 +1,46 @@
+// Membuat objek `animal` dengan metode `speak`
 const animal = {
   speak() {
     console.log(`${this.name} makes a noise.`);
   },
 };
+
+// Membuat objek `dog` yang mewarisi dari `animal`
 const dog = Object.create(animal);
-dog.name = "Beki";
-dog.speak(); // Output: Beki makes a noise.
+dog.name = "Beki"; // Menambahkan properti `name` ke `dog`
+dog.speak(); // Output: "Beki makes a noise."
+// `dog` memanggil metode `speak`, yang diambil dari prototype `animal`
 
-console.log(dog.__proto__ === animal);
+// Memeriksa apakah `dog.__proto__` sama dengan `animal`
+console.log(dog.__proto__ === animal); // Output: true
+// Ini berarti `dog` mewarisi properti dan metode dari `animal`
 
+// Membuat objek `cat` yang juga mewarisi dari `animal`
 const cat = Object.create(animal);
-cat.name = "Tom";
-cat.speak(); // Output: Tom makes a noise.
+cat.name = "Tom"; // Menambahkan properti `name` ke `cat`
+cat.speak(); // Output: "Tom makes a noise."
+// Sama seperti `dog`, `cat` juga memanggil metode `speak` dari prototype `animal`
 
+// Menimpa metode `speak` hanya untuk objek `dog`
 dog.speak = function () {
   console.log(`${this.name} barks.`);
 };
-dog.speak(); // Output: Beki barks.
+dog.speak(); // Output: "Beki barks."
+// Metode `speak` pada `dog` sekarang telah diubah, sehingga memanggil versi barunya sendiri
 
-//menambahkan properti baru pada object animal
-
+// Menambahkan properti baru `legs` ke `animal`
 animal.legs = 4;
 
+// Menambahkan properti `legs` ke `dog`
 dog.legs = 4;
 console.log(dog.legs); // Output: 4
+// `dog` memiliki properti `legs` sendiri, sehingga tidak mengambil dari `animal`
 
-//task murid
+// Menambahkan metode baru `run` ke `animal`
 animal.run = function () {
   console.log(`${this.name} is running.`);
 };
 
-dog.run(); // Output: Beki is running.
+// Memanggil metode `run` dari `dog`
+dog.run(); // Output: "Beki is running."
+// `dog` tidak memiliki metode `run` sendiri, tetapi mewarisinya dari `animal`
--- 3.PrototypalInheritance/b.Inheritance-with-Prototype/index.js
+++ 3.PrototypalInheritance/b.Inheritance-with-Prototype/index.js
@@ -1,19 +1,33 @@
+// Membuat konstruktor `Mammal`
 function Mammal(name) {
-  this.name = name;
+  this.name = name; // Properti `name` ditambahkan ke instance yang dibuat
 }
 
+// Menambahkan metode `speak` ke `Mammal.prototype`
 Mammal.prototype.speak = function () {
   console.log(`${this.name} is speaking.`);
 };
+// Semua instance `Mammal` dan turunan dari `Mammal` akan memiliki akses ke metode ini
 
+// Membuat konstruktor `Dog` yang mewarisi dari `Mammal`
 function Dog(name, breed) {
-  Mammal.call(this, name);
-  this.breed = breed;
+  // Memanggil konstruktor `Mammal`, supaya `name` diwarisi oleh `Dog`
+  Mammal.call(this, name); // Memanggil `Mammal` dengan `this` agar properti `name` bisa diakses di `Dog`
+  this.breed = breed; // Menambahkan properti `breed` khusus untuk `Dog`
 }
 
+// Mengatur prototype `Dog` agar mewarisi dari `Mammal.prototype`
 Dog.prototype = Object.create(Mammal.prototype);
+
+// Memperbaiki constructor `Dog`, karena sebelumnya `Dog.prototype.constructor` mengarah ke `Mammal`
 Dog.prototype.constructor = Dog;
 
+// Menambahkan metode baru `bark` ke `Dog.prototype`
 Dog.prototype.bark = function () {
   console.log(`${this.name} is barking.`);
 };
+
+// Sekarang, setiap instance `Dog` akan memiliki:
+// - Properti `name` dan `breed`
+// - Metode `speak()` dari `Mammal`
+// - Metode `bark()` dari `Dog`
